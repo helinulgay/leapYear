d7f58ff603acdd9cbbb4a89d14cf40fb83d0061b
hlnul <hlnul@LAPTOP-JKL9KSV4>
2023-05-24 15:28:47 +0300

    leapYear

---
+++ src/ornekler/artıkYıl.java
@@ -0,0 +1,24 @@
+package ornekler;
+
+import java.util.Scanner;
+
+public class artıkYıl {
+
+	public static void main(String[] args) {
+		while(true) {
+		Scanner scan=new Scanner(System.in);
+		System.out.println("enter a year:");
+		int year=scan.nextInt();
+		
+
+		        if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
+		            System.out.println(year + " is a leap year.");
+		        } else {
+		            System.out.println(year + " is not a leap year.");
+		        }
+		    }
+		}
+
+	}
+
+
